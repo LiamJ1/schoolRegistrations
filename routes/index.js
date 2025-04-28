a419597e70a97e3b415b323d88193d051799cc0f
c2dt <liamjacobsen41@gmail.com>
2025-04-28 09:39:57 -0700

    Revert "INTIA"
    This reverts commit 9f45b6baca644e5fbca2f3546e752b657b343603.

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const courseController = require('../controllers/courseController');
-const studentController = require('../controllers/studentController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/courses', courseController.viewAll);
-router.get('/courses/profile/:id', courseController.viewProfile);
-router.get('/courses/edit/:id', courseController.renderEditForm)
-router.get('/courses/add',courseController.renderAddForm)
-router.post('/courses/add',courseController.addCourse)
-router.get('/courses/delete/:id',courseController.deleteCourse)
-
-
-router.get('/students', studentController.viewAll);
-router.get('/students/profile/:id', studentController.viewProfile);
-router.get('/student/edit/:id', studentController.renderEditForm);
-router.get('/students/add', studentController.renderAddForm);
-router.post('/students/add', studentController.addStudent)
-router.get('/students/delete/:id', studentController.deleteStudent)
-router.post('/students/:studentId/enroll', studentController.enrollStudent);
-router.get('/students/:studentId/removeCourse/:courseId', studentController.removeCourse);
-router.post('/courses/:courseId/enroll', courseController.enrollStudent);
-router.get('/courses/:courseId/removeStudent/:studentId',courseController.removeStudent);
 
 module.exports = router;
